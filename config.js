149d24b354cb08ec00e0666fab3960cb366bc61e
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-30 12:34:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Kırkçeşme  Suyolları / Kırkçeşme Waterways',
+    title: 'Suyun Metabolik Akışı: Kırkçeşme Suyolu / Metabolic Flow of Water: Kırkçeşme Waterway',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
